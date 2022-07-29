92b9b403dfbbeca86c14b6603697f1bb2924d17d
ProstoSanya27 <prosto.sanya27@gmail.com>
2022-07-29 17:08:13 +0300

    test

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,9 +1,9 @@
 const multer  = require('multer')
 
-module.exports = multer({
+module.exports = console.log('--upload middl--')/*multer({
 	//storage: multer.memoryStorage(),
 	dest: './uploads/',
 	limits: {
 		fileSize: 1048576 // 1 MB
 	}
-}).single('filedata')
+}).single('filedata')*/
