dffede38e15bde4dc4c8daf4014ef8e28f39f2d6
ProstoSanya27 <prosto.sanya27@gmail.com>
2022-07-30 10:36:37 +0300

    del upload.js

--- middlewares/upload.js
+++
@@ -1,9 +0,0 @@
-//const multer  = require('multer')
-
-module.exports = console.log('--upload middl v2--')/*multer({
-	//storage: multer.memoryStorage(),
-	dest: './uploads/',
-	limits: {
-		fileSize: 1048576 // 1 MB
-	}
-}).single('filedata')*/
