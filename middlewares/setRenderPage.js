b9ceeb64987f641a53fc90dca4a33a11e8e021db
ProstoSanya27 <prosto.sanya27@gmail.com>
2022-08-02 16:09:50 +0300

    images view

--- middlewares/setRenderPage.js
+++ middlewares/setRenderPage.js
@@ -2,7 +2,17 @@ const setRenderPage = (req, res, next) => {
   res.set({ 'Content-Type': 'text/html; charset=utf-8' })
   res.renderPage = (tpl, params) => res.render(
     tpl,
-    Object.assign({error: '', message: '', loggedin: false, images: []}, req.session, params)
+    Object.assign(
+      {
+        error: '',
+        message: '',
+        loggedin: false,
+        images: [],
+        bucket_url: 'https://storage.googleapis.com/' + process.env.GCLOUD_STORAGE_BUCKET
+      },
+      req.session,
+      params
+    )
   )
   next()
 }
